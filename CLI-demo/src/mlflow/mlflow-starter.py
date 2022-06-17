b3548ebc376bd44f96f382061391112a08ffb750
lindacmsheard <22198777+lindacmsheard@users.noreply.github.com>
2022-06-17 13:41:14 +0000

    demo updates

--- CLI-demo/src/mlflow/mlflow-starter.py
+++ CLI-demo/src/mlflow/mlflow-starter.py
@@ -7,9 +7,12 @@ from random import random
 
 # define functions
 def main():
+    # some other code related to ML
+    mlflow.log_param("my_custom", "my value for Hyosun")
     mlflow.log_param("python_version", sys.version)
     mlflow.log_param("hello_param", "world")
     mlflow.log_metric("hello_metric", random())
+
     os.system(f"echo 'hello world' > helloworld.txt")
     mlflow.log_artifact("helloworld.txt")
 
